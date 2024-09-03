5bd3a0ca5a6a704f1927d7501de32518c49be0bb
MikaelDv <enzomikael@hotmail.com>
2024-09-03 10:04:42 -0300

    faz o L

--- aula08/src/components/Rodape/Rodape.tsx
+++ aula08/src/components/Rodape/Rodape.tsx
@@ -1,7 +1,7 @@
 export default function Rodape(){
     return(
         <footer>
-            <p>Desenvolvido por <a href="https://github.com/luiz">Rei Luizinho</a></p>
+            <p>Desenvolvido por <a href="https://github.com/MikaelDv">Mikael</a></p>
         </footer>
     )
 }
